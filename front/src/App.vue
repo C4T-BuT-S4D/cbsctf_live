9fc406e547696d1b74516c093bdc23f6955d95a4
xmikasax <xmikasax.main@gmail.com>
2020-09-19 16:33:38 +0300

    Move to vue component

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,4 +1,18 @@
 <template>
-  <div id="app">
-  </div>
+    <div id="app">
+        <vue-telegram-login 
+            mode="callback"
+            telegram-login="cbsctf_regbot"
+            @callback="telegramAuth" />
+    </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        telegramAuth: async function(data) {
+            console.log(data);
+        }
+    }
+}
+</script>
