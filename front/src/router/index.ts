5bc7c32473acc857fa179eb38e34e9e3eac13c6f
xmikasax <xmikasax.main@gmail.com>
2020-09-25 14:41:32 +0300

    Build

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,14 +1,16 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 
+import Index from '@/pages/Index.vue';
+
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  // {
-  //   path: "/",
-  //   name: "Home",
-  //   component: Home
-  // },
+  {
+    path: "/",
+    name: "Index",
+    component: Index
+  },
 ];
 
 const router = new VueRouter({
