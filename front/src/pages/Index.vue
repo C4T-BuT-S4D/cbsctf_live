da52b573977566b63dbe42c80b5fd1481cfd237e
Roman Nikitin <nikrom2012@me.com>
2020-09-25 14:54:48 +0300

    Fix url

--- front/src/pages/Index.vue
+++ front/src/pages/Index.vue
@@ -66,7 +66,7 @@ export default {
     },
 
     created: async function () {
-        this.url = window.location.origin + '/telegram';
+        this.url = window.location.origin + window.location.pathname + '/telegram';
     },
 
     mounted: async function () {
