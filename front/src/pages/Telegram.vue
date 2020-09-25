f12a1cec4bb90287bd3889065f1ec272d12dc2ab
Roman Nikitin <nikrom2012@me.com>
2020-09-25 15:17:48 +0300

    ID & auth date converted to int

--- front/src/pages/Telegram.vue
+++ front/src/pages/Telegram.vue
@@ -8,7 +8,11 @@ export default {
         console.log('Called created!');
         console.log(this.$route.query);
 
-        const { data } = await this.$http.post('/telegram/callback/', this.$route.query);
+        const user = this.$route.query;
+        user['id'] = parseInt(user['id']);
+        user['auth_date'] = parseInt(user['auth_date']);
+
+        const { data } = await this.$http.post('/telegram/callback/', user);
         console.log('Got server response');
         console.log(data);
 
