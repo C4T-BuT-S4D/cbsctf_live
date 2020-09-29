f210bdd9d176787923414922f97c6f31015f0dc9
Roman Nikitin <nikrom2012@me.com>
2020-09-30 01:08:32 +0300

    Better deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,13 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 cd "$DIR/front"
 yarn build
 cp "$DIR/front/dist/index.html" "$DIR/front/dist/404.html"
+printf "register.cbsctf.live" > "$DIR/front/dist/CNAME"
 
 cd "$DIR"
 git add front
 git commit -m "$1"
 git subtree push --prefix front/dist origin gh-pages
+
+# for sync
+git subtree pull --prefix front/dist origin gh-pages
+
